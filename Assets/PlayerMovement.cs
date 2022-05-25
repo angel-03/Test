22654ff6ffbc5dab11a1b2261cad75b5e9bd5baf
angel_03 <angel20dsouza00@gmail.com>
2022-05-25 15:31:32 +0530

    first

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public GameObject gameObject;
+    private float direction;
+    public float jumpForce;
+    public Rigidbody2D body;
+    public Animator animator;
+    private bool isRight;
+    private bool isGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(direction));
+         if(isRight && direction > 0f)
+        {
+            transform.localScale = new Vector3(1,1,1);
+            isRight = false;
+        }
+        else if(!isRight && direction < 0f)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+            isRight = true;
+        }
+        /*if(Input.GetKeyDown(KeyCode.A))
+        {
+            body.velocity = new Vector2(-4,0) *speed*Time.deltaTime;
+            gameObject.transform.localScale = new Vector3(-1, transform.localScale.y);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            body.velocity = new Vector2(4,0)*speed*Time.deltaTime;
+            gameObject.transform.localScale = new Vector2(1, transform.localScale.y);
+        }*/
+    }
+
+    void FixedUpdate() 
+    {
+        direction = Input.GetAxisRaw("Horizontal");
+        gameObject.transform.Translate(new Vector3(direction,0,0) * speed * Time.deltaTime);
+        //animator.SetBool("isJump",false);
+        if (Input.GetButtonDown("Jump")&& Mathf.Abs(body.velocity.y) < 0.01f)
+        {
+            isGround = false;
+            animator.SetBool("isJump",true);
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.tag == "gameOver")    
+        {
+            Debug.Log("GameOver!!!!");
+            SceneManager.LoadScene(0);
+        }
+        if(other.gameObject.tag == "Platform")
+        {
+            isGround = true;
+            animator.SetBool("isJump",false);
+        }
+    }
+}
---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    private int heath = 10;
+    private int currentHeath;
+    [Range (0,100)]  
+    public int age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Game!!!");
+        StartCoroutine("CheckAge");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Key Pressed");
+        }
+        
+    }
+    void FixedUpdate()
+    {
+        if(Input.GetAxisRaw("Horizontal") == 1 )
+        {
+            Debug.Log("Right Key Pressed");
+        }
+        
+    }
+    public void Vote(int age)
+    {
+        this.age = age;
+        if(age > 18 && age < 70)
+        {
+            Debug.Log("Can vote");
+        }
+        else 
+        {
+            Debug.Log("Cant vote");
+        }
+    }
+
+    IEnumerator CheckAge()
+    {
+        Vote(age);
+        yield return new WaitForSeconds(2f);
+        Vote(age);
+        yield return new WaitForSeconds(2f);
+        StartCoroutine("CheckAge");
+        yield return new WaitForSeconds(4f);
+
+    }
+}
